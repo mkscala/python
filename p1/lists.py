77ed2b7aec20456efc300b0e8478f5775a19c736
Richm <ric5hme45@yahoo.com>
2018-02-11 17:30:45 +0200

    list

--- p1/lists.py
+++ p1/lists.py
@@ -1,3 +1,21 @@
+
+
+
+# Loops and Lists  106     'learn python the hard way'
+
+the_count = [1, 2, 3, 4, 5]
+fruits = ['apples', 'oranges', 'pears', 'apricots']
+change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
+
+# this first kind of for- loop goes through a list
+
+for number in the_count:
+    print("This is count %d" % number)
+# same as above
+for fruit in fruits:
+    print("A fruit of type: %s" % fruit)
+# Loops and Lists  107     'learn python the hard way'
+
 # https://www.youtube.com/watch?v=A1HUzrvS-Pw
 # http://www.newthinktank.com/2016/07/learn-program-6/
 
