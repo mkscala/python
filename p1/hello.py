091744807e65e09e5dcf2ed66821af269cf3c0bd
Your Name <you@example.com>
2018-06-08 21:54:55 +0300

    amiop

---
+++ p1/hello.py
@@ -0,0 +1,14 @@
+__author__ = "book"
+print('you say', )
+
+
+def main():
+    name = input('Name')
+    print('Hello ' + name)
+
+
+print('Goodbye')
+
+if __name__ == '__main__':
+    print('I say',)
+    main()
